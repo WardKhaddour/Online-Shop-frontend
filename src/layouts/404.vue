d20dbeb46dc58ea0dee1d4900d5b249f3d298000
WardKhaddour <wardkhaddot@gmail.com>
2022-09-23 02:26:37 +0300

    add 404 page

---
+++ src/layouts/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <main>
+    <h1 class="centered">Oops! Page Not Found</h1>
+
+    <router-link to="/" class="link btn">Go Home</router-link>
+  </main>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Error404Layout from "@/layouts/404";
 import MainLayout from "@/layouts/MainLayout";
 import ShopModuleRoutes from "@/modules/shop/routes";
 import AdminModuleRoutes from "@/modules/admin/routes";
-
 Vue.use(Router);
 
 const router = new Router({
@@ -18,6 +18,10 @@ const router = new Router({
       path: "/admin",
       component: MainLayout,
       children: [AdminModuleRoutes]
+    },
+    {
+      path: "*",
+      component: Error404Layout
     }
   ]
 });
